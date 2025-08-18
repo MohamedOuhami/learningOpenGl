2952faefd992edf9b81e051edf079519c7c772cd
MohamedOuhami <mohamed.ouhami2001@gmail.com>
2025-08-18 11:18:38 +0100

    Started learning

---
+++ build/Desktop-Debug/CMakeFiles/LearningOpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearningOpenGL.dir/link.d"
+  "CMakeFiles/LearningOpenGL.dir/main.cpp.o"
+  "CMakeFiles/LearningOpenGL.dir/main.cpp.o.d"
+  "LearningOpenGL"
+  "LearningOpenGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LearningOpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
